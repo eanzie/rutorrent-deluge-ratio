74bbec3ca588104201c79fb20f9a61ac6448188c
eanzie <eanzie@protonmail.com>
2022-02-21 19:45:46 -0800

    round ratio to 3 decimal points, update README

--- README.md
+++ README.md
@@ -1,8 +1,28 @@
 # rutorrent-deluge-ratio
 ruTorrent plugin to show Deluge Ratio
 
-This is a plug that will add 2 columns to ruTorrent.  The DlgRatio and the TotalRatio.
+This is a plug that will add 2 additional columns to ruTorrent UI so that there are 3 columns for ratio.
 
-This is used for transfering data from deluge into rtorrent, but don't want to lose the stats.
+1. **Ratio** - Ratio since adding to rtorrent
+2. **DlgRatio** - Custom field that hold the ratio prior to moving from Deluge
+3. **TotalRatio** - Ratio + DlgRatio
+
+I use rtorrent load.start with additional custom field
+
+```js
+[
+        {key: 'd.directory.set', value: download_location}, // current path to data
+        {key: 'd.custom.set', value: 'deluge_ratio,' + ratio}, // deluge ratio
+        {key: 'd.custom.set', value: 'deluge_completed,' + completed_time} // deluge completed time
+];
+```
+
+https://rtorrent-docs.readthedocs.io/en/latest/cmd-ref.html#term-load-start
+
+---
+Original Readme
+
+
+This is used for transferring data from deluge into rtorrent, but don't want to lose the stats.
 
 This plugin was originally copied from the seedingtime plugin, then modified.  I don't know much about JS or PHP, so this is what I came up with.
--- init.js
+++ init.js
@@ -1,73 +1,58 @@
 plugin.loadLang();
 
-if(plugin.canChangeColumns())
-{
+if (plugin.canChangeColumns()) {
 	plugin.config = theWebUI.config;
-	theWebUI.config = function(data)
-	{
+	theWebUI.config = function (data) {
 		this.tables.trt.columns.push({text: 'DlgRatio', width: '100px', id: 'dratio', type: TYPE_NUMBER});
 		this.tables.trt.columns.push({text: 'TotalRatio', width: '100px', id: 'tratio', type: TYPE_NUMBER});
 		plugin.trtFormat = this.tables.trt.format;
-		plugin.config.call(this,data);
-		plugin.reqId1 = theRequestManager.addRequest("trt", theRequestManager.map("d.get_custom=")+"deluge_ratio",function(hash,torrent,value)
-		{
-			torrent.dratio = value;
+		plugin.config.call(this, data);
+		plugin.reqId1 = theRequestManager.addRequest("trt", theRequestManager.map("d.get_custom=") + "deluge_ratio", function (hash, torrent, value) {
+			torrent.dratio = Number(value).toFixed(3);
 		});
-		plugin.reqId2 = theRequestManager.addRequest("trt", theRequestManager.map("d.get_ratio="),function(hash,torrent,value)
-		{
-			total = Number(value)/1000 + Number(torrent.dratio);
+		plugin.reqId2 = theRequestManager.addRequest("trt", theRequestManager.map("d.get_ratio="), function (hash, torrent, value) {
+			const total = Number(value) / 1000 + Number(torrent.dratio);
 			torrent.tratio = +total.toFixed(3);
 		});
 		plugin.trtRenameColumn();
 	}
 
-	plugin.trtRenameColumn = function()
-	{
-		if(plugin.allStuffLoaded)
-		{
-			theWebUI.getTable("trt").renameColumnById("dratio",theUILang.dratio);
-			theWebUI.getTable("trt").renameColumnById("tratio",theUILang.tratio);
-			if(thePlugins.isInstalled("rss"))
+	plugin.trtRenameColumn = function () {
+		if (plugin.allStuffLoaded) {
+			theWebUI.getTable("trt").renameColumnById("dratio", theUILang.dratio);
+			theWebUI.getTable("trt").renameColumnById("tratio", theUILang.tratio);
+			if (thePlugins.isInstalled("rss"))
 				plugin.rssRenameColumn();
-			if(thePlugins.isInstalled("extsearch"))
+			if (thePlugins.isInstalled("extsearch"))
 				plugin.tegRenameColumn();
-		}
-		else
-			setTimeout(arguments.callee,1000);
+		} else
+			setTimeout(arguments.callee, 1000);
 	}
 
-	plugin.rssRenameColumn = function()
-	{
-		if(theWebUI.getTable("rss").created)
-		{
-			theWebUI.getTable("rss").renameColumnById("dratio",theUILang.dratio);
-			theWebUI.getTable("rss").renameColumnById("tratio",theUILang.tratio);
-		}
-		else
-			setTimeout(arguments.callee,1000);
+	plugin.rssRenameColumn = function () {
+		if (theWebUI.getTable("rss").created) {
+			theWebUI.getTable("rss").renameColumnById("dratio", theUILang.dratio);
+			theWebUI.getTable("rss").renameColumnById("tratio", theUILang.tratio);
+		} else
+			setTimeout(arguments.callee, 1000);
 	}
 
-	plugin.tegRenameColumn = function()
-	{
-		if(theWebUI.getTable("teg").created)
-		{
-			theWebUI.getTable("teg").renameColumnById("dratio",theUILang.dratio);
-			theWebUI.getTable("teg").renameColumnById("tratio",theUILang.tratio);
-		}
-		else
-			setTimeout(arguments.callee,1000);
+	plugin.tegRenameColumn = function () {
+		if (theWebUI.getTable("teg").created) {
+			theWebUI.getTable("teg").renameColumnById("dratio", theUILang.dratio);
+			theWebUI.getTable("teg").renameColumnById("tratio", theUILang.tratio);
+		} else
+			setTimeout(arguments.callee, 1000);
 	}
 }
 
-plugin.onRemove = function()
-{
+plugin.onRemove = function () {
 	theWebUI.getTable("trt").removeColumnById("dratio");
 	theWebUI.getTable("trt").removeColumnById("tratio");
-	if(thePlugins.isInstalled("rss"))
-	{
+	if (thePlugins.isInstalled("rss")) {
 		theWebUI.getTable("rss").removeColumnById("dratio");
 		theWebUI.getTable("rss").removeColumnById("tratio");
 	}
-	theRequestManager.removeRequest( "trt", plugin.reqId1 );
-	theRequestManager.removeRequest( "trt", plugin.reqId2 );
+	theRequestManager.removeRequest("trt", plugin.reqId1);
+	theRequestManager.removeRequest("trt", plugin.reqId2);
 }
